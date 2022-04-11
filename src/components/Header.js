c8626ba76c7d08164caf58e33a3e8acb710dda19
Jacquier jean-Marie <jm.jacquier@gmail.com>
2022-04-11 17:27:52 +0200

    work in progress

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './Header.css';
 
 export default function Header() {
   return (
     <header>
-      <nav>
-        <Link to="/">Accueil</Link>
-        <Link to="/about">A propos</Link>
-        <Link to="/journal">Journal</Link>
-        <Link to="/road">Parcours</Link>
+      <nav className="navBar">
+        <div className="logoMeteo">LogoMeteo</div>
+        <div className="logoShoe">LogoShoe</div>
+        <Link className="Navlink" to="/">
+          Accueil
+        </Link>
+        <Link className="Navlink" to="/about">
+          A propos
+        </Link>
+        <Link className="Navlink" to="/journal">
+          Journal
+        </Link>
+        <Link className="Navlink" to="/road">
+          Parcours
+        </Link>
       </nav>
     </header>
   );
