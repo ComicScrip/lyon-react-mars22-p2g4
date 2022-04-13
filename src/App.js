fb797f50eb58afb43c2a2f6ac23b589972ac9a04
Jacquier jean-Marie <jm.jacquier@gmail.com>
2022-04-13 16:43:50 +0200

    nav bar avancement

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Footer from './components/Footer';
 import Header from './components/Header';
 import Main from './components/Main';
+import IconMenu from './components/IconMenu';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Header />
       <Main />
       <Footer />
+      <IconMenu />
     </>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,26 +1,71 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './Header.css';
+/* eslint-disable no-undef */
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import { red } from 'material-ui-colors';
+import { CloudQueue } from '@mui/icons-material';
 
-export default function Header() {
+export default function ButtonAppBar() {
   return (
-    <header>
-      <nav className="navBar">
-        <div className="logoMeteo">LogoMeteo</div>
-        <div className="logoShoe">LogoShoe</div>
-        <Link className="Navlink" to="/">
-          Accueil
-        </Link>
-        <Link className="Navlink" to="/about">
-          A propos
-        </Link>
-        <Link className="Navlink" to="/journal">
-          Journal
-        </Link>
-        <Link className="Navlink" to="/road">
-          Parcours
-        </Link>
-      </nav>
-    </header>
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar style={{ backgroundColor: red[500] }}>
+          <CloudQueue fontSize="large" />
+
+          <Typography
+            textAlign="center"
+            variant="h6"
+            component="div"
+            sx={{ flexGrow: 1 }}
+          >
+            Pic'n'Move
+          </Typography>
+
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+    </Box>
   );
 }
+
+// export default function Header() {
+//   return (
+//     <header>
+//       <nav className="navBar">
+//         <div>
+//           <img
+//             src="img/logo_meteo.png"
+//             alt="notrelogométéo"
+//             width="40"
+//             height="40"
+//           />
+//         </div>
+//         <div className="logoShoe">LogoShoe</div>
+//         <Link className="Navlink" to="/">
+//           Accueil
+//         </Link>
+//         <Link className="Navlink" to="/about">
+//           A propos
+//         </Link>
+//         <Link className="Navlink" to="/journal">
+//           Journal
+//         </Link>
+//         <Link className="Navlink" to="/road">
+//           Parcours
+//         </Link>
+//       </nav>
+//     </header>
+//   );
+// }
