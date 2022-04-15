e1b1cedc7bab20c18035d2a01ea598a06a55891a
Jacquier jean-Marie <jm.jacquier@gmail.com>
2022-04-15 14:00:33 +0200

    work in progress

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 /* eslint-disable no-undef */
 import * as React from 'react';
+import { useState } from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
@@ -8,66 +9,106 @@ import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
 import { red } from 'material-ui-colors';
 import { CloudQueue } from '@mui/icons-material';
-import IceSkatingIcon from '@mui/icons-material/IceSkating';
 
-export default function ButtonAppBar() {
+import Divider from '@mui/material/Divider';
+import Paper from '@mui/material/Paper';
+import MenuList from '@mui/material/MenuList';
+import MenuItem from '@mui/material/MenuItem';
+import ListItemText from '@mui/material/ListItemText';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import Home from '@mui/icons-material/Home';
+import EditRoad from '@mui/icons-material/EditRoad';
+import MenuBook from '@mui/icons-material/MenuBook';
+import Info from '@mui/icons-material/Info';
+
+export default function Header() {
+  const [toggleState, setToggleState] = useState(false);
+
+  const toggleFunction = () => {
+    setToggleState(!toggleState);
+  };
+
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
-        <Toolbar style={{ backgroundColor: red[500] }}>
-          <CloudQueue fontSize="large" />
+    <div>
+      <div>User</div>
+      <button type="button" onClick={toggleFunction}>
+        Click
+      </button>
+      {toggleState ? (
+        <div>toto</div>
+      ) : (
+        <Box sx={{ flexGrow: 1 }}>
+          <AppBar position="static">
+            <Toolbar style={{ backgroundColor: red[500] }}>
+              <CloudQueue fontSize="large" />
 
-          <Typography
-            textAlign="center"
-            variant="h6"
-            component="div"
-            sx={{ flexGrow: 1 }}
-          >
-            Pic'n'Move
-          </Typography>
-          <IceSkatingIcon fontSize="large" />
+              <Typography
+                textAlign="center"
+                variant="h6"
+                component="div"
+                sx={{ flexGrow: 1 }}
+              >
+                Pic'n'Move
+              </Typography>
 
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
-        </Toolbar>
-      </AppBar>
-    </Box>
+              <IconButton
+                size="large"
+                edge="start"
+                color="inherit"
+                aria-label="menu"
+                sx={{ mr: 2 }}
+              >
+                <Paper sx={{ width: 320, maxWidth: '100%' }}>
+                  <MenuList>
+                    <MenuItem>
+                      <ListItemIcon>
+                        <Home fontSize="small" />
+                      </ListItemIcon>
+                      <ListItemText>Home</ListItemText>
+                      <Typography variant="body2" color="text.secondary">
+                        ⌘X
+                      </Typography>
+                    </MenuItem>
+
+                    <MenuItem>
+                      <ListItemIcon>
+                        <EditRoad fontSize="small" />
+                      </ListItemIcon>
+                      <ListItemText>Road</ListItemText>
+                      <Typography variant="body2" color="text.secondary">
+                        ⌘C
+                      </Typography>
+                    </MenuItem>
+
+                    <MenuItem>
+                      <ListItemIcon>
+                        <MenuBook fontSize="small" />
+                      </ListItemIcon>
+                      <ListItemText>Journal</ListItemText>
+                      <Typography variant="body2" color="text.secondary">
+                        ⌘V
+                      </Typography>
+                    </MenuItem>
+
+                    <MenuItem>
+                      <ListItemIcon>
+                        <Info fontSize="small" />
+                      </ListItemIcon>
+                      <ListItemText>About</ListItemText>
+                      <Typography variant="body2" color="text.secondary">
+                        ⌘F
+                      </Typography>
+                    </MenuItem>
+
+                    <Divider />
+                  </MenuList>
+                </Paper>
+                <MenuIcon />
+              </IconButton>
+            </Toolbar>
+          </AppBar>
+        </Box>
+      )}
+    </div>
   );
 }
-
-// export default function Header() {
-//   return (
-//     <header>
-//       <nav className="navBar">
-//         <div>
-//           <img
-//             src="img/logo_meteo.png"
-//             alt="notrelogométéo"
-//             width="40"
-//             height="40"
-//           />
-//         </div>
-//         <div className="logoShoe">LogoShoe</div>
-//         <Link className="Navlink" to="/">
-//           Accueil
-//         </Link>
-//         <Link className="Navlink" to="/about">
-//           A propos
-//         </Link>
-//         <Link className="Navlink" to="/journal">
-//           Journal
-//         </Link>
-//         <Link className="Navlink" to="/road">
-//           Parcours
-//         </Link>
-//       </nav>
-//     </header>
-//   );
-// }
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,5 +1,38 @@
-import React from 'react';
+import { MenuItem, MenuList, Paper } from '@mui/material';
+import { useState } from 'react';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import { EditRoad } from '@mui/icons-material';
 
 export default function Home() {
-  return <div>Home</div>;
+  const [toggleState, setToggleState] = useState(false);
+
+  const toggleFunction = () => {
+    setToggleState(!toggleState);
+  };
+
+  return (
+    <div>
+      <div>User</div>
+      <button type="button" onClick={toggleFunction}>
+        Click
+      </button>
+      {toggleState ? (
+        <div>toto</div>
+      ) : (
+        <Paper>
+          {' '}
+          <MenuList>
+            {' '}
+            <MenuItem>
+              {' '}
+              <ListItemIcon>
+                {' '}
+                <EditRoad fontSize="small" />{' '}
+              </ListItemIcon>{' '}
+            </MenuItem>{' '}
+          </MenuList>{' '}
+        </Paper>
+      )}
+    </div>
+  );
 }
