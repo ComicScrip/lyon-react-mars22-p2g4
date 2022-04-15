92eab6561071812bab2c08f0d1ce5f76872a293c
ChrichriR <chriss_s@hotmail.fr>
2022-04-15 11:11:50 +0200

    create a MUi button

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,5 +1,23 @@
 import React from 'react';
+// import ReactDOM from 'react-dom';
+import Button from '@mui/material/Button';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { red } from '@mui/material/colors';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: red[700],
+    },
+  },
+});
 
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <ThemeProvider theme={theme}>
+      <Button variant="contained" size="large">
+        Let's GO !
+      </Button>
+    </ThemeProvider>
+  );
 }
