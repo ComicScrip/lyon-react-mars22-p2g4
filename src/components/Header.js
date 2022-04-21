d95a5bd2d0157f132b0425675d906aba5d3d5148
Jacquier jean-Marie <jm.jacquier@gmail.com>
2022-04-21 16:45:52 +0200

    finalisation navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,15 +9,16 @@ function Header() {
   };
   return (
     <nav className={`navbar ${showLinks ? 'show-nav' : 'hide-nav'}`}>
+      <img className="meteo" alt="" src="meteo.png" />
       <div className="navbar_logo">
-        <img className="meteo" alt="" src="meteo.png" />
-        <img className="logo" alt="" src="shoe.png" />
+        <div className="container">
+          <h1>PIC 'N' MOVE</h1>
+          <img className="logo" alt="" src="shoe.svg" />
+        </div>
       </div>
+
       <ul className="navbar_links">
         <li className="navbar_items">
-          {/* <a href="/" className="navbar_link">
-            Accueil
-          </a> */}
           <Link to="/" className="navbar_link">
             Accueil
           </Link>
@@ -26,33 +27,18 @@ function Header() {
           <Link to="/road" className="navbar_link">
             Parcours
           </Link>
-          {/* <a href="/" className="navbar_link">
-            Créer un parcours
-          </a> */}
         </li>
-
-        {/* <li className="navbar_items">
-          <a href="/" className="navbar_link">
-            Partir courir
-          </a>
-        </li> */}
 
         <li className="navbar_items">
           <Link to="/journal" className="navbar_link">
             Journal
           </Link>
-          {/* <a href="/" className="navbar_link">
-            Mon historique
-          </a> */}
         </li>
 
         <li className="navbar_items">
           <Link to="/about" className="navbar_link">
             À propos
           </Link>
-          {/* <a href="/" className="navbar_link">
-            Contact
-          </a> */}
         </li>
       </ul>
       <button type="button" className="navbar_burger" onClick={handleShowLinks}>
