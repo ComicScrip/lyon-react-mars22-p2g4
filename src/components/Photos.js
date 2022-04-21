7e82df9656579b6fe0e91cac7d90fb55c875dd01
fflachat <florent.flachat@gmail.com>
2022-04-21 15:21:59 +0200

    fix display issue with adding promise.all

--- src/components/Photos.js
+++ src/components/Photos.js
@@ -2,15 +2,16 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import parse from 'html-react-parser';
+import TestGeoJSON from '../ressources/lyon_touristic.json';
 
 // API Call parameter
 const apiKey = process.env.REACT_APP_FLICKR_APIKEY;
-const radius = '0.1';
+const radius = '0.2';
 const safeSearch = '1';
 const contentType = '1';
 const minUploadDate = '2000-01-01 00:00:01';
 const perPage = '100';
-const tags = 'street_art';
+const tags = 'street-art';
 const currentPosition = { lat: 45.746156, lon: 4.827308 };
 
 // Get geolocalisation (logitude, latitude)
@@ -36,16 +37,22 @@ const defaultPhotosList = [
   },
 ];
 
-const pickRandomPics = (picsList, picsNb) => {
-  const newPicsList = [];
-  for (let i = 0; i < picsNb; i += 1) {
-    const randomIndex = Math.floor(Math.random() * picsList.length);
-    newPicsList.push(picsList[randomIndex]);
-  }
+const pickRandomPic = (picsList) => {
+  const randomIndex = Math.floor(Math.random() * picsList.length);
+  const newPicsList = picsList[randomIndex];
   return newPicsList;
 };
 
-export default function Photos() {
+const reducePathPoints = (GeoJSONFile, factor = 10) => {
+  const CoordinatesTab = [];
+  const coordinatesPathTab = GeoJSONFile.features[0].geometry.coordinates;
+  for (let i = 0; i < coordinatesPathTab.length / factor; i += 1) {
+    CoordinatesTab.push(coordinatesPathTab[factor * i]);
+  }
+  return CoordinatesTab;
+};
+
+export default function PhotosWIP() {
   const [photosList, setPhotosList] = useState(defaultPhotosList);
   const [loadingError, setLoadingError] = useState();
   const [isLoading, setIsLoading] = useState(false);
@@ -53,17 +60,38 @@ export default function Photos() {
   const getPhotos = () => {
     setLoadingError('');
     setIsLoading(true);
-    // Send the request
-    axios
-      .get(
-        `https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&radius=${radius}&safe_search=${safeSearch}&content_type=${contentType}&lat=${currentPosition.lat}&lon=${currentPosition.lon}&per_page=${perPage}&min_taken_date=${minUploadDate}&tags=${tags}&extras=description,geo&format=json&nojsoncallback=1`
-      )
-      // Extract the DATA from the received response
-      .then((response) => response.data)
-      // Use this data to update the state
-      .then((data) => data.photos)
-      .then((photos) => pickRandomPics(photos.photo, 1))
-      .then((randomPhotosList) => setPhotosList(randomPhotosList))
+
+    const coordinatesTab = reducePathPoints(TestGeoJSON);
+    console.log(coordinatesTab);
+    const promisesList = [];
+
+    for (let i = 0; i < coordinatesTab.length; i += 1) {
+      const lat = coordinatesTab[i][1];
+      const lon = coordinatesTab[i][0];
+
+      const newPromise = new Promise((resolve) => {
+        axios
+          .get(
+            `https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&radius=${radius}&safe_search=${safeSearch}&content_type=${contentType}&lat=${lat}&lon=${lon}&per_page=${perPage}&min_taken_date=${minUploadDate}&tags=${tags}&extras=description,geo&format=json&nojsoncallback=1`
+          )
+          .then((response) => response.data)
+          .then((data) => data.photos)
+          .then((photos) => pickRandomPic(photos.photo))
+          .then((randomPhoto) => {
+            if (randomPhoto !== undefined) {
+              resolve(randomPhoto);
+            } else resolve(null);
+            console.log(randomPhoto);
+          });
+      });
+      console.log(newPromise);
+      promisesList.push(newPromise);
+    }
+
+    console.log(promisesList);
+
+    Promise.all(promisesList)
+      .then((randomPhotos) => setPhotosList(randomPhotos))
       .catch((err) => {
         console.error(err);
         setLoadingError("Impossible de charger les photos depuis l'API");
@@ -84,15 +112,19 @@ export default function Photos() {
       </button>
 
       <div className="displayPhotos">
-        {photosList.map((p) => (
-          <div key={p.id}>
-            <img
-              src={`https://live.staticflickr.com/${p.server}/${p.id}_${p.secret}_z.jpg`}
-              alt=""
-            />
-            <p>{parse(p.description._content)}</p>
-          </div>
-        ))}
+        {photosList.map(
+          (p) =>
+            // eslint-disable-next-line implicit-arrow-linebreak
+            p && (
+              <div key={p.id}>
+                <img
+                  src={`https://live.staticflickr.com/${p.server}/${p.id}_${p.secret}_z.jpg`}
+                  alt=""
+                />
+                <p key={p.id}>{parse(p.description._content)}</p>
+              </div>
+            )
+        )}
       </div>
     </div>
   );
