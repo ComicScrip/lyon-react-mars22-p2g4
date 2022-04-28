ef5e2f51922cd3f34ab825338c373b9c4502e387
fflachat <florent.flachat@gmail.com>
2022-04-28 12:05:01 +0200

    adapt path card to desktop version

--- src/components/PathCard.js
+++ src/components/PathCard.js
@@ -1,25 +1,24 @@
 /* eslint-disable object-curly-newline */
 import React from 'react';
-import '../screens/Paths.css';
+import './PathCard.css';
 import { Link } from 'react-router-dom';
 
 export default function PathCard({ title, pathMap, distance, time }) {
   return (
     <Link to="/pathdetails" className="cardHistoryContainer">
-      <h1 className="namePath">{title}</h1>
-      <img
-        src={pathMap}
-        className="pathMap w-50 h-20"
-        alt={`map of ${title}`}
-      />
-      <p className="distancePath">
-        Distance : <br />
-        {distance}
-      </p>
-      <p className="timePath">
-        Temps : <br />
-        {time}
-      </p>
+      <img src={pathMap} className="pathMap" alt={`map of ${title}`} />
+      <div className="pathCardInfo">
+        <h1 className="namePath">{title}</h1>
+
+        <p className="distancePath">
+          Distance : <br />
+          {distance}
+        </p>
+        <p className="timePath">
+          Temps : <br />
+          {time}
+        </p>
+      </div>
     </Link>
   );
 }
