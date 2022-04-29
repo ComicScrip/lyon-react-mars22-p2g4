d73b4034842fb52f16b5958929625daf72082b01
fflachat <florent.flachat@gmail.com>
2022-04-29 14:13:03 +0200

    change pathcard

--- src/components/PathCard.js
+++ src/components/PathCard.js
@@ -1,25 +1,11 @@
 /* eslint-disable object-curly-newline */
 import './PathCard.css';
 import { Link } from 'react-router-dom';
-import DisplayMap from './DisplayMap';
 
-export default function PathCard({
-  id,
-  title,
-  pathMap,
-  distance,
-  elevation,
-  startingLat,
-  startingLon,
-}) {
+export default function PathCard({ id, title, distance, elevation }) {
   return (
     <Link to={`/pathdetails/${id}`} className="cardHistoryContainer">
       <div className="pathCardInfo">
-        <DisplayMap
-          className="pathMap"
-          path={pathMap}
-          position={[startingLat, startingLon]}
-        />
         <h1 className="namePath">{title}</h1>
         <p className="distancePath">
           Distance : <br />
