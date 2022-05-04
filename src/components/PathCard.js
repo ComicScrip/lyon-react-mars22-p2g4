d4cf0c54010cc875bca31e58ec034cd82bc7cdc1
fflachat <florent.flachat@gmail.com>
2022-05-04 11:49:13 +0200

    try to fix css pb, add  pathCard.css file

--- src/components/PathCard.js
+++ src/components/PathCard.js
@@ -1,4 +1,5 @@
 import { Link } from 'react-router-dom';
+import style from './PathCard.module.css';
 
 const accessToken = process.env.REACT_APP_MAPBOX_TOKEN;
 
@@ -12,19 +13,19 @@ export default function PathCard({
 }) {
   return (
     <Link to={`/pathdetails/${id}`}>
-      <div className="pathCardContainer">
+      <div className={style.pathCardContainer}>
         <img
           src={`https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/static/${startLon},${startLat},13,0/400x400?before_layer=airport-label&access_token=${accessToken}`}
           alt={`parcours : ${title}`}
-          className="pathMap"
+          className={style.pathMap}
         />
-        <div className="pathCardInfo">
-          <h2 className="pathName">{title}</h2>
-          <div className="pathDistance">
+        <div className={style.pathCardInfo}>
+          <h2 className={style.pathName}>{title}</h2>
+          <div className={style.pathDistance}>
             Distance : <br />
             {distance} km
           </div>
-          <div className="pathElevation">
+          <div className={style.pathElevation}>
             Dénivelé : <br />
             {elevation} m
           </div>
